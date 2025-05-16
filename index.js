3b0920df9f503bcc62d924f51f1956b2bc4829da
nazaporta <nazarena@porta.com.py>
2025-05-16 19:16:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Backend MovieApp is running");
+});
+app.get("/api/movies", async (req, res) => {
+  const { movie } = req.query;
+  const apiKey = process.env.TMD_API_KEY;
+
+  if (!movie) {
+    return res.status(400).json({ error: "Movie parameter is required" });
+  }
+
+  try {
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/search/movie",
+      {
+        params: {
+          query: movie,
+          api_key: apiKey,
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching movie data:", error.message);
+    res.status(500).json({ error: "Error fetching movie data" });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
